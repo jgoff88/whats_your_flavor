b9ff5b634cb39c10b4d852c1d99f5fc56e13ba9a
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-01-21 21:16:10 -0600

    added schema for deployment to heroku

--- models/schema.sql
+++ models/schema.sql
@@ -2,11 +2,11 @@ CREATE DATABASE whats_your_flavor;
 USE whats_your_flavor;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `first_name` text NOT NULL,
-  `last_name` text NOT NULL,
-  `mob_no` int(11) NOT NULL,
-  `user_name` varchar(20) NOT NULL,
-  `password` varchar(15) NOT NULL,
+  id int(5) NOT NULL AUTO_INCREMENT,
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  mob_no int(11) NOT NULL,
+  user_name varchar(20) NOT NULL,
+  password varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ); 
