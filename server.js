8308de75108230d0865e02ff756bb53f3e01aa18
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-23 19:22:43 -0600

    updated routes and dependencies

--- server.js
+++ server.js
@@ -1,47 +1,52 @@
-require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
-
-var db = require("./models");
-
 var app = express();
-var PORT = process.env.PORT || 3000;
+var passport = require("passport");
+var session = require("express-session");
+var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
+//Parse application body
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-// Handlebars
+// For Passport
+// session secret
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//For Handlebars
+app.set("views", "./app/views");
 app.engine(
-  "handlebars",
+  "hbs",
   exphbs({
-    defaultLayout: "main"
+    extname: ".hbs"
   })
 );
-app.set("view engine", "handlebars");
+app.set("view engine", ".hbs");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.get("/", function(req, res) {
+  res.send("Welcome to Passport with Sequelize");
 });
 
-module.exports = app;
+//Models
+var models = require("./app/models");
+
+//Routes
+var authRoute = require("./app/routes/auth.js")(app, passport);
+//load passport strategies
+require("./app/config/passport/passport.js")(passport, models.user);
+
+// Sync Database and listen to local server
+models.sequelize
+  .sync()
+  .then(function() {
+    app.listen(PORT, function(err) {
+      console.log("Server is running on port 5000 and database looks fine");
+    });
+  })
+  .catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!");
+  });
