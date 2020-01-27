470e99d88ec1a19d7c7610d9b7dbfe588f6e4693
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-27 14:43:34 -0600

    created initial file

---
+++ app/data/coffee.js
@@ -0,0 +1,7 @@
+var coffees = [
+  {
+    name: "Mocha Frappuccino",
+    photo: (src = "app/assets/images/Mocha Frappuccino.jpeg"),
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  }
+];
