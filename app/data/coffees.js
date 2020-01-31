b8affa9b35db804ebb349f18d7a0a77eb635016d
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-30 18:52:31 -0600

    updated

--- app/data/coffees.js
+++ app/data/coffees.js
@@ -2,7 +2,7 @@ var coffees = [
   {
     name: "Mocha Frappuccino",
     photo: "public/img/mocha frappuccino.jpeg",
-    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
   }
 ];
 
